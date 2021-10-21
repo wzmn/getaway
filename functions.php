233a29d0ee557d72dc3a7a5e278eafec29d4ec6f
Wiseman Fernandes <wizyman@gmail.com>
2021-10-21 09:05:34 +0530

    enquire now section

--- functions.php
+++ functions.php
@@ -48,13 +48,35 @@ if ( ! function_exists( 'getaway_qodef_call_button' ) ) {
 	 */
 	function getaway_qodef_call_button() {
 		if ( getaway_qodef_options()->getOptionValue( 'show_back_button' ) == 'yes' ) { ?>
-            <a id='fixed-call' href='#'>
+            <a id='fixed-call' onclick="this.nextElementSibling.classList.add('open')">
                 <span class="qodef-icon-stack">
 					<i class="qodef-icon-font-awesome fa <?php echo getaway_qodef_icon_collections()->iconCollections['font_awesome']->icons['fa-phone']; ?> "></i>
                 </span>
             </a>
+            <div class="fixed-enquire-now">
+            	<?php dynamic_sidebar( 'enquire_now' ); ?>
+            	<span class="close" onclick="this.parentElement.classList.remove('open')">
+            		<i class="fa fa-times qodef-icon-font-awesome"></i>
+            	</span>
+            </div>
 		<?php }
 	}
 
 	add_action( 'getaway_qodef_after_header_area', 'getaway_qodef_call_button', 10 );
 }
+
+function register_enquire_now_widget(){
+	register_sidebar(
+		array(
+			'id'            => 'enquire_now',
+			'name'          => esc_html__( 'Enquire Now', 'getaway' ),
+			'description'   => esc_html__( 'Widgets added here will appear when you click the call icon', 'getaway' ),
+			'before_widget' => '',
+			'after_widget'  => '',
+			'before_title'  => '',
+			'after_title'   => ''
+		)
+	);	
+}
+
+add_action( 'widgets_init', 'register_enquire_now_widget' );
