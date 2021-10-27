8f0d4411ffbd04974baff52753640fee4a8d2af0
Wiseman Fernandes <wizyman@gmail.com>
2021-10-27 20:25:10 +0530

    another change

--- functions.php
+++ functions.php
@@ -50,7 +50,7 @@ if ( ! function_exists( 'getaway_qodef_call_button' ) ) {
 		if ( getaway_qodef_options()->getOptionValue( 'show_back_button' ) == 'yes' ) { ?>
             <a id='fixed-call' onclick="this.nextElementSibling.classList.add('open')">
                 <span class="qodef-icon-stack">
-					<i class="qodef-icon-font-awesome fa <?php echo getaway_qodef_icon_collections()->iconCollections['font_awesome']->icons['fa-phone']; ?> "></i>
+					<i class="qodef-icon-font-awesome fa <?php echo getaway_qodef_icon_collections()->iconCollections['font_awesome']->icons['fa-comments']; ?> "></i>
                 </span>
             </a>
             <div class="fixed-enquire-now">
